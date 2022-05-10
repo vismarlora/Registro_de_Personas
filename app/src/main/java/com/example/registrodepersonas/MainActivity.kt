34685e29ba67d10beef6aeb27c9349e7dc56a40f
vismarlora <vismar_lora@ucne.edu.do>
2022-05-10 15:31:17 -0400

    Enlazando el viewModel con el MainActivity.kt y agregando las variables de binding

--- app/src/main/java/com/example/registrodepersonas/MainActivity.kt
+++ app/src/main/java/com/example/registrodepersonas/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.registrodepersonas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.example.registrodepersonas.databinding.ActivityMainBinding
+import com.example.registrodepersonas.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get()
+        viewModel.iniciar()
     }
 }
