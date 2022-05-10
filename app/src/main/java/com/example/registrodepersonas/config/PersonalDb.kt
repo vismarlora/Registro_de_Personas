96c9c125fdcf1295fbac6d533c33a6c1c90840c4
vismarlora <vismar_lora@ucne.edu.do>
2022-05-10 14:23:13 -0400

    Configurando la Base de Datos

--- app/src/main/java/com/example/registrodepersonas/config/PersonalDb.kt
+++ app/src/main/java/com/example/registrodepersonas/config/PersonalDb.kt
@@ -1,12 +1,15 @@
 package com.example.registrodepersonas.config
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.registrodepersonas.dao.PersonalDao
 import com.example.registrodepersonas.models.Personal
 
 @Database(
     entities = [Personal::class],
     version = 1
 )
-abstract class PersonalDb {
+abstract class PersonalDb:RoomDatabase() {
+    abstract fun personalDao():PersonalDao
 
 }
