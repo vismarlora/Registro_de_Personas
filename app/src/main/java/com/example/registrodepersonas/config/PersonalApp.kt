2936c0dda9ceb464a91b7682b0fbcea2c8ad4a2e
vismarlora <vismar_lora@ucne.edu.do>
2022-05-10 14:43:35 -0400

    Creando la clase PersonalApp para cuando la aplicacion inicie se cree

---
+++ app/src/main/java/com/example/registrodepersonas/config/PersonalApp.kt
@@ -0,0 +1,19 @@
+package com.example.registrodepersonas.config
+
+import android.app.Application
+import androidx.room.Room
+
+class PersonalApp:Application() {
+    companion object{
+        lateinit var db: PersonalDb
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        db = Room.databaseBuilder(
+            this,
+            PersonalDb::class.java,
+            "personal"
+        ).build()
+    }
+}
