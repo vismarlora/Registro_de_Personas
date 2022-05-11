86fce09442cb7c96b92d7389f017c64988f77325
vismarlora <vismar_lora@ucne.edu.do>
2022-05-11 16:22:39 -0400

    Inicializando las variables y agregando los bindings en el FormularioActivity.kt

--- app/src/main/java/com/example/registrodepersonas/ui/FormularioActivity.kt
+++ app/src/main/java/com/example/registrodepersonas/ui/FormularioActivity.kt
@@ -2,11 +2,27 @@ package com.example.registrodepersonas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.example.registrodepersonas.R
+import com.example.registrodepersonas.config.Constantes
+import com.example.registrodepersonas.databinding.ActivityFormularioBinding
+import com.example.registrodepersonas.databinding.ItemListBinding
+import com.example.registrodepersonas.viewmodel.FormularioViewModel
 
 class FormularioActivity : AppCompatActivity() {
+    lateinit var binding:ActivityFormularioBinding
+    lateinit var viewModel:FormularioViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario)
+        binding = ActivityFormularioBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get()
+        viewModel.operacion = intent.getStringExtra(Constantes.OPERACION_KEY)!!
+        binding.modelo = viewModel
+        binding.lifecycleOwner = this
     }
 }
