27db93fd4f4495b2dc82684955d1887e09ddff78
vismarlora <vismar_lora@ucne.edu.do>
2022-05-11 12:54:44 -0400

    Creando algunas constantes para editar a una persona

---
+++ app/src/main/java/com/example/registrodepersonas/config/Constantes.kt
@@ -0,0 +1,8 @@
+package com.example.registrodepersonas.config
+
+object Constantes {
+
+    val OPERACION_KEY = "OPERACION_KEY"
+    val OPERACION_INSERTAR = "INSERTAR"
+    val OPERACION_EDITAR = "EDITAR"
+}
