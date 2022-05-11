abcba595443f18333e61ee6e2e8bac68cdc91adb
vismarlora <vismar_lora@ucne.edu.do>
2022-05-11 15:04:19 -0400

    Realizando algunos cambios en PersonalDao.kt

--- app/src/main/java/com/example/registrodepersonas/dao/PersonalDao.kt
+++ app/src/main/java/com/example/registrodepersonas/dao/PersonalDao.kt
@@ -7,15 +7,15 @@ import com.example.registrodepersonas.models.Personal
 @Dao
 interface PersonalDao {
     @Query("SELECT * FROM Personal")
-    suspend fun getAll(): List<Personal>
+    fun getAll(): List<Personal>
 
     @Insert
-    suspend fun insert(personas: List<Personal>):List<Long>
+    fun insert(personas: List<Personal>):List<Long>
 
     @Update
-    suspend fun update(personal:Personal):Int
+    fun update(personal:Personal):Int
 
     @Delete
-    suspend fun delete(personal: Personal):Int
+    fun delete(personal: Personal):Int
 
 }
